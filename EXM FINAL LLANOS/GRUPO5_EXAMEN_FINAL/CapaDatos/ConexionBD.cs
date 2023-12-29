e52c9f7722216818b110fd6a8ef742e11da17fd5
Walter-Llanos-0 <152580809+Walter-Llanos-0@users.noreply.github.com>
2023-12-28 20:47:20 -0500

    Add files via upload

---
+++ EXM FINAL LLANOS/GRUPO5_EXAMEN_FINAL/CapaDatos/ConexionBD.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class ConexionBD
+    {
+        SqlConnection cn = new SqlConnection("Data Source=LAPTOP-B9COPN0L;Database=Grupo_5;User ID=sa;Password=1234");
+
+        public SqlConnection conectar()
+        {
+            return cn;
+        }
+
+        public void abrir()
+        {
+            cn.Open();
+        }
+
+        public void cerrar()
+        {
+            cn.Close();
+        }
+    }
+}
