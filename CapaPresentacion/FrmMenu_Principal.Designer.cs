195438271127d2951e120031f02f1d104e3ad78e
Walter-Llanos-0 <152580809+Walter-Llanos-0@users.noreply.github.com>
2024-01-03 16:57:29 -0500

    Add files via upload

---
+++ CapaPresentacion/FrmMenu_Principal.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace CapaPresentacion
+{
+    partial class FrmMenu_Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.usuarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registroUsuarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registroClienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.movimientoClienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.proveedorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registroProveedorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.movimientoProveedorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuarioToolStripMenuItem,
+            this.clienteToolStripMenuItem,
+            this.proveedorToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1520, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // usuarioToolStripMenuItem
+            // 
+            this.usuarioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registroUsuarioToolStripMenuItem});
+            this.usuarioToolStripMenuItem.Name = "usuarioToolStripMenuItem";
+            this.usuarioToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.usuarioToolStripMenuItem.Text = "Usuario";
+            // 
+            // registroUsuarioToolStripMenuItem
+            // 
+            this.registroUsuarioToolStripMenuItem.Name = "registroUsuarioToolStripMenuItem";
+            this.registroUsuarioToolStripMenuItem.Size = new System.Drawing.Size(201, 26);
+            this.registroUsuarioToolStripMenuItem.Text = "Registro Usuario";
+            this.registroUsuarioToolStripMenuItem.Click += new System.EventHandler(this.registroUsuarioToolStripMenuItem_Click);
+            // 
+            // clienteToolStripMenuItem
+            // 
+            this.clienteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registroClienteToolStripMenuItem,
+            this.movimientoClienteToolStripMenuItem});
+            this.clienteToolStripMenuItem.Name = "clienteToolStripMenuItem";
+            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
+            this.clienteToolStripMenuItem.Text = "Cliente";
+            // 
+            // registroClienteToolStripMenuItem
+            // 
+            this.registroClienteToolStripMenuItem.Name = "registroClienteToolStripMenuItem";
+            this.registroClienteToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.registroClienteToolStripMenuItem.Text = "Registro Cliente";
+            this.registroClienteToolStripMenuItem.Click += new System.EventHandler(this.registroClienteToolStripMenuItem_Click);
+            // 
+            // movimientoClienteToolStripMenuItem
+            // 
+            this.movimientoClienteToolStripMenuItem.Name = "movimientoClienteToolStripMenuItem";
+            this.movimientoClienteToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.movimientoClienteToolStripMenuItem.Text = "Movimiento Cliente";
+            this.movimientoClienteToolStripMenuItem.Click += new System.EventHandler(this.movimientoClienteToolStripMenuItem_Click);
+            // 
+            // proveedorToolStripMenuItem
+            // 
+            this.proveedorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registroProveedorToolStripMenuItem,
+            this.movimientoProveedorToolStripMenuItem});
+            this.proveedorToolStripMenuItem.Name = "proveedorToolStripMenuItem";
+            this.proveedorToolStripMenuItem.Size = new System.Drawing.Size(91, 24);
+            this.proveedorToolStripMenuItem.Text = "Proveedor";
+            // 
+            // registroProveedorToolStripMenuItem
+            // 
+            this.registroProveedorToolStripMenuItem.Name = "registroProveedorToolStripMenuItem";
+            this.registroProveedorToolStripMenuItem.Size = new System.Drawing.Size(244, 26);
+            this.registroProveedorToolStripMenuItem.Text = "Registro Proveedor";
+            this.registroProveedorToolStripMenuItem.Click += new System.EventHandler(this.registroProveedorToolStripMenuItem_Click);
+            // 
+            // movimientoProveedorToolStripMenuItem
+            // 
+            this.movimientoProveedorToolStripMenuItem.Name = "movimientoProveedorToolStripMenuItem";
+            this.movimientoProveedorToolStripMenuItem.Size = new System.Drawing.Size(244, 26);
+            this.movimientoProveedorToolStripMenuItem.Text = "Movimiento Proveedor";
+            this.movimientoProveedorToolStripMenuItem.Click += new System.EventHandler(this.movimientoProveedorToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(52, 24);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // FrmMenu_Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1520, 763);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmMenu_Principal";
+            this.Text = "FrmMenu_Principal";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem usuarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registroUsuarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registroClienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem movimientoClienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem proveedorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registroProveedorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem movimientoProveedorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+    }
+}
---
+++ Entidades/R_Proovedor.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class R_Proovedor
+    {
+        public int idProveedor { get; set; }
+        public string tipopro { get; set; }
+        public string tipodoc { get; set; }
+        public string numerodoc { get; set; }
+        public string apepat { get; set; }
+        public string apemat { get; set; }
+        public string nombre { get; set; }
+        public string ruc { get; set; }
+        public string razon { get; set; }
+        public string domicilio { get; set; }
+        public string departamento { get; set; }
+        public string provincia { get; set; }
+        public string distrito { get; set; }
+
+        public string telefono { get; set; }
+        public string contacto { get; set; }
+        public string correo { get; set; }
+        public string banco { get; set; }
+
+        public int nrocuenta { get; set; }
+        public int nrointer { get; set; }
+        public string estado { get; set; }
+
+        //colocando constructores
+
+        public R_Proovedor(int idproovedor, string tipopro, string tipodoc, string numerodoc, string apepat, string apemat,
+            string nombre, string ruc, string razon, string domicilio, string departamento, string provincia, string distrito, string telefono,
+            string contacto, string correo, string banco, int nrocuenta, int nrointer, string estado)
+        {
+            this.idProveedor = idproovedor;
+            this.tipopro = tipopro;
+            this.tipodoc = tipodoc;
+            this.numerodoc = numerodoc;
+            this.apepat = apepat;
+            this.apemat = apemat;
+            this.nombre = nombre;
+            this.ruc = ruc;
+            this.razon = razon;
+            this.domicilio = domicilio;
+            this.departamento = departamento;
+            this.provincia = provincia;
+            this.distrito = distrito;
+            this.telefono = telefono;
+            this.contacto = contacto;
+            this.correo = correo;
+            this.banco = banco;
+            this.nrocuenta = nrocuenta;
+            this.nrointer = nrointer;
+            this.estado = estado;
+        }
+
+        public R_Proovedor(string tipopro, string tipodoc, string numerodoc, string apepat, string apemat, string nombre,
+            string ruc, string razon, string domicilio, string departamento, string provincia, string distrito, string telefono,
+            string contacto, string correo, string banco, int nrocuenta, int nrointer, string estado)
+        {
+
+            this.tipopro = tipopro;
+            this.tipodoc = tipodoc;
+            this.numerodoc = numerodoc;
+            this.apepat = apepat;
+            this.apemat = apemat;
+            this.nombre = nombre;
+            this.ruc = ruc;
+            this.razon = razon;
+            this.domicilio = domicilio;
+            this.departamento = departamento;
+            this.provincia = provincia;
+            this.distrito = distrito;
+            this.telefono = telefono;
+            this.contacto = contacto;
+            this.correo = correo;
+            this.banco = banco;
+            this.nrocuenta = nrocuenta;
+            this.nrointer = nrointer;
+            this.estado = estado;
+
+
+        }
+
+        public R_Proovedor()
+        {
+
+        }
+    }
+}
