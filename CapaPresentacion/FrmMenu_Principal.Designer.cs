160f5cba8cffd6a406ca2d1b6949b515464df4f9
Walter-Llanos-0 <152580809+Walter-Llanos-0@users.noreply.github.com>
2024-01-03 15:58:26 -0500

    Add files via upload

---
+++ CapaPresentacion/FrmMenu_Principal.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CapaPresentacion
+{
+    partial class FrmMenu_Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.usuarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registroUsuarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registroClienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.movimientoClienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.proveedorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registroProveedorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.movimientoProveedorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuarioToolStripMenuItem,
+            this.clienteToolStripMenuItem,
+            this.proveedorToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1140, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // usuarioToolStripMenuItem
+            // 
+            this.usuarioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registroUsuarioToolStripMenuItem});
+            this.usuarioToolStripMenuItem.Name = "usuarioToolStripMenuItem";
+            this.usuarioToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.usuarioToolStripMenuItem.Text = "Usuario";
+            // 
+            // registroUsuarioToolStripMenuItem
+            // 
+            this.registroUsuarioToolStripMenuItem.Name = "registroUsuarioToolStripMenuItem";
+            this.registroUsuarioToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.registroUsuarioToolStripMenuItem.Text = "Registro Usuario";
+            this.registroUsuarioToolStripMenuItem.Click += new System.EventHandler(this.registroUsuarioToolStripMenuItem_Click);
+            // 
+            // clienteToolStripMenuItem
+            // 
+            this.clienteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registroClienteToolStripMenuItem,
+            this.movimientoClienteToolStripMenuItem});
+            this.clienteToolStripMenuItem.Name = "clienteToolStripMenuItem";
+            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.clienteToolStripMenuItem.Text = "Cliente";
+            // 
+            // registroClienteToolStripMenuItem
+            // 
+            this.registroClienteToolStripMenuItem.Name = "registroClienteToolStripMenuItem";
+            this.registroClienteToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.registroClienteToolStripMenuItem.Text = "Registro Cliente";
+            // 
+            // movimientoClienteToolStripMenuItem
+            // 
+            this.movimientoClienteToolStripMenuItem.Name = "movimientoClienteToolStripMenuItem";
+            this.movimientoClienteToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.movimientoClienteToolStripMenuItem.Text = "Movimiento Cliente";
+            // 
+            // proveedorToolStripMenuItem
+            // 
+            this.proveedorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registroProveedorToolStripMenuItem,
+            this.movimientoProveedorToolStripMenuItem});
+            this.proveedorToolStripMenuItem.Name = "proveedorToolStripMenuItem";
+            this.proveedorToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.proveedorToolStripMenuItem.Text = "Proveedor";
+            // 
+            // registroProveedorToolStripMenuItem
+            // 
+            this.registroProveedorToolStripMenuItem.Name = "registroProveedorToolStripMenuItem";
+            this.registroProveedorToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.registroProveedorToolStripMenuItem.Text = "Registro Proveedor";
+            this.registroProveedorToolStripMenuItem.Click += new System.EventHandler(this.registroProveedorToolStripMenuItem_Click);
+            // 
+            // movimientoProveedorToolStripMenuItem
+            // 
+            this.movimientoProveedorToolStripMenuItem.Name = "movimientoProveedorToolStripMenuItem";
+            this.movimientoProveedorToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.movimientoProveedorToolStripMenuItem.Text = "Movimiento Proveedor";
+            this.movimientoProveedorToolStripMenuItem.Click += new System.EventHandler(this.movimientoProveedorToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // FrmMenu_Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1140, 620);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMenu_Principal";
+            this.Text = "FrmMenu_Principal";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem usuarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registroUsuarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registroClienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem movimientoClienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem proveedorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registroProveedorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem movimientoProveedorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+    }
+}
---
+++ CapaPresentacion/FrmRegistro_Usuario.cs
@@ -0,0 +1,207 @@
+﻿using CapaNegocio;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class FrmRegistro_Usuario : Form
+    {
+        R_UsuarioBL or_usuarioBL = new R_UsuarioBL();
+
+        String ruta = "";
+
+
+        // GUARDAR PERSONAL PARA EL BOTONO  : AGREGAR 
+
+        SqlConnection cn = new SqlConnection("Data Source=DESKTOP-2T10M4M;Initial Catalog=Grupo_5;Persist Security Info=True;User ID=sa;Password=74994175");
+        void GuardarPersonal(string usu, string cont, string apepa, string apema, string nom, string perf, string esta, string fotos)
+        {
+            try
+            {
+                MemoryStream ms = new MemoryStream();
+                FileStream fs = new FileStream(fotos, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                ms.SetLength(fs.Length);
+                fs.Read(ms.GetBuffer(), 0, (int)fs.Length);
+                byte[] arrImg = ms.GetBuffer();
+                ms.Flush();
+                fs.Close();
+
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cn.Open();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "sp_InsertarRegistro_Usuario";
+                    cmd.Parameters.AddWithValue("@p_usuario", usu);
+                    cmd.Parameters.AddWithValue("@p_contrasenia", cont);
+                    cmd.Parameters.AddWithValue("@p_apepaterno", apepa);
+                    cmd.Parameters.AddWithValue("@p_apematerno", apema);
+                    cmd.Parameters.AddWithValue("@p_nombre", nom);
+                    cmd.Parameters.AddWithValue("@p_perfil", perf);
+                    cmd.Parameters.AddWithValue("@p_estado", esta);
+                    cmd.Parameters.Add("@p_FotoUsuario", SqlDbType.VarBinary).Value = arrImg;
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public FrmRegistro_Usuario()
+        {
+            InitializeComponent();
+            tblRegistro_Usuario.DataSource = or_usuarioBL.listarUsuario();
+        }
+
+        void LimpiarUsuario()
+        {
+            txtIdUsuario.Clear();
+            txtUsuario1.Clear();
+            txtContrasenia1.Clear();
+            txtApePaterno.Clear();
+            txtApeMaterno.Clear();
+            txtNombre.Clear();
+            cboPerfil.SelectedIndex = (0);
+            cboEstado.SelectedIndex = (0);
+            
+
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            LimpiarUsuario();
+            txtUsuario1.Focus();
+        }
+
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (pbFoto.Image == null || ruta == "")
+            {
+                MessageBox.Show("Falta seleccionar la foto", "Atencion");
+            }
+            else
+            {
+                try
+                {
+                    GuardarPersonal(txtUsuario1.Text, txtContrasenia1.Text, txtApePaterno.Text, txtApeMaterno.Text, txtNombre.Text, cboPerfil.SelectedItem.ToString(), cboEstado.SelectedItem.ToString(), ruta);
+                    MessageBox.Show("Se guardo satisfactoriamente", "Atencion");
+                    tblRegistro_Usuario.DataSource = or_usuarioBL.listarUsuario();
+                    cboEstado.ResetText();
+                    cboPerfil.ResetText();
+                    txtNombre.ResetText();
+                    txtApeMaterno.ResetText();
+                    txtApePaterno.ResetText();
+                    txtContrasenia1.ResetText();
+                    txtUsuario1.ResetText();
+                    pbFoto.Image = null;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            Int16 idusuario = Int16.Parse(Microsoft.VisualBasic.Interaction.InputBox("Codigo a buscar"));
+
+            try
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cn.Open();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "sp_BuscarRegistro_Usuario";
+                    cmd.Parameters.AddWithValue("@p_idusuario", idusuario);
+                    SqlDataReader dr;
+                    dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        txtUsuario1.Text = dr.GetString(1);
+                        txtContrasenia1.Text = dr.GetString(2);
+                        txtApePaterno.Text = dr.GetString(3);
+                        txtApeMaterno.Text = dr.GetString(4);
+                        txtNombre.Text = dr.GetString(5);
+                        cboPerfil.Text = dr.GetString(6);
+                        cboEstado.Text = dr.GetString(7);
+                        byte[] arrImg = (byte[])dr.GetSqlBinary(8);
+                        MemoryStream ms = new MemoryStream(arrImg);
+                        Image img = Image.FromStream(ms);
+                        pbFoto.Image = img;
+                        pbFoto.SizeMode = PictureBoxSizeMode.StretchImage;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario no EXISTE", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    dr.Close();
+                    cn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            string men;
+            men = or_usuarioBL.actualizarUsuario(new R_Usuario(Int16.Parse(txtIdUsuario.Text), txtUsuario1.Text, txtContrasenia1.Text, txtApePaterno.Text, txtApeMaterno.Text, txtNombre.Text, cboPerfil.SelectedItem.ToString(), cboEstado.SelectedItem.ToString(), ruta));
+
+            MessageBox.Show(men);
+            tblRegistro_Usuario.DataSource = or_usuarioBL.listarUsuario();
+            LimpiarUsuario();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            string men;
+            men = or_usuarioBL.eliminarUsuario(Int16.Parse(txtIdUsuario.Text));
+            MessageBox.Show(men);
+            tblRegistro_Usuario.DataSource = or_usuarioBL.listarUsuario();
+            LimpiarUsuario();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarUsuario();
+
+        }
+
+        private void btnSeleccionarImagen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofdAbrir = new OpenFileDialog();
+            ofdAbrir.Filter = "*.bmp;*.gif;*.jpg;*.png|*.bmp;*.gif;*.jpg;*.png";
+            ofdAbrir.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            ofdAbrir.Title = "Seleccionar la imagen";
+            ofdAbrir.RestoreDirectory = true;
+
+            if (ofdAbrir.ShowDialog() == DialogResult.OK)
+            {
+                ruta = ofdAbrir.FileName;
+
+                pbFoto.Image = Image.FromFile(ruta);
+                pbFoto.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                ruta = "";
+                pbFoto.Image = null;
+            }
+        }
+    }
+}
