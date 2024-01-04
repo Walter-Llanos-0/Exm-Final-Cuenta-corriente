01830cd5761fe6bb471441f117a9c919287e424f
Walter Llanos <llanoswalter10@gmail.com>
2024-01-03 22:28:42 -0500

    fin

--- Examen_Final_Grupo5/CapaPresentacion/FrmLogin.Designer.cs
+++ Examen_Final_Grupo5/CapaPresentacion/FrmLogin.Designer.cs
@@ -30,13 +30,13 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtcontraseña = new System.Windows.Forms.TextBox();
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,9 +44,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(223, 29);
+            this.label1.Location = new System.Drawing.Point(297, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(245, 24);
+            this.label1.Size = new System.Drawing.Size(310, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "CUENTAS CORRIENTES";
             // 
@@ -55,85 +56,94 @@
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.txtcontraseña);
+            this.panel1.Controls.Add(this.txtusuario);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(33, 28);
+            this.panel1.Location = new System.Drawing.Point(44, 34);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(614, 316);
+            this.panel1.Size = new System.Drawing.Size(819, 389);
             this.panel1.TabIndex = 1;
             // 
-            // label2
+            // button3
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(43, 87);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 24);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "USUARIO";
+            this.button3.Location = new System.Drawing.Point(597, 281);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 66);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "LIMPIAR";
+            this.button3.UseVisualStyleBackColor = true;
             // 
-            // label3
+            // button2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(43, 164);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(152, 24);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "CONTRASEÑA";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(227, 87);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(287, 20);
-            this.textBox1.TabIndex = 3;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(227, 164);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(287, 20);
-            this.textBox2.TabIndex = 4;
-            this.textBox2.UseSystemPasswordChar = true;
+            this.button2.Location = new System.Drawing.Point(351, 281);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 66);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "CANCELAR";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(77, 228);
+            this.button1.Location = new System.Drawing.Point(103, 281);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 54);
+            this.button1.Size = new System.Drawing.Size(185, 66);
             this.button1.TabIndex = 5;
             this.button1.Text = "ACEPTAR";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // txtcontraseña
             // 
-            this.button2.Location = new System.Drawing.Point(263, 228);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(131, 54);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "CANCELAR";
-            this.button2.UseVisualStyleBackColor = true;
+            this.txtcontraseña.Location = new System.Drawing.Point(303, 202);
+            this.txtcontraseña.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtcontraseña.Name = "txtcontraseña";
+            this.txtcontraseña.Size = new System.Drawing.Size(381, 22);
+            this.txtcontraseña.TabIndex = 4;
+            this.txtcontraseña.UseSystemPasswordChar = true;
             // 
-            // button3
+            // txtusuario
             // 
-            this.button3.Location = new System.Drawing.Point(448, 228);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(113, 54);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "LIMPIAR";
-            this.button3.UseVisualStyleBackColor = true;
+            this.txtusuario.Location = new System.Drawing.Point(303, 107);
+            this.txtusuario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(381, 22);
+            this.txtusuario.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(57, 202);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(190, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CONTRASEÑA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(57, 107);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "USUARIO";
             // 
             // FrmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(685, 369);
+            this.ClientSize = new System.Drawing.Size(913, 454);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmLogin";
             this.Text = "Inicio de Usuario";
             this.panel1.ResumeLayout(false);
@@ -146,8 +156,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtcontraseña;
+        private System.Windows.Forms.TextBox txtusuario;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button3;
--- Examen_Final_Grupo5/CapaPresentacion/FrmLogin.cs
+++ Examen_Final_Grupo5/CapaPresentacion/FrmLogin.cs
@@ -7,11 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaNegocio;
+
+
 
 namespace CapaPresentacion
 {
     public partial class FrmLogin : Form
     {
+
+        R_UsuarioBL or_usuariobl = new R_UsuarioBL();
+
+        string Usuario1 = "adrian";
+        string Usuario2 = "kevin";
+        string Usuario3 = "walter";
         public FrmLogin()
         {
             InitializeComponent();
@@ -19,8 +28,17 @@ namespace CapaPresentacion
 
         private void button1_Click(object sender, EventArgs e)
         {
-            FrmMenu_Principal menu_Principal = new FrmMenu_Principal();
-            menu_Principal.ShowDialog();
+            if (txtusuario.Text == Usuario1 && txtusuario.Text == Usuario2 && txtusuario.Text == Usuario3)
+            {
+                txtusuario.Clear();
+                txtcontraseña.Clear();
+                FrmMenu_Principal menu = new FrmMenu_Principal();
+                menu.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Usuario Incorrecto", "Atencion");
+            }
         }
     }
 }
